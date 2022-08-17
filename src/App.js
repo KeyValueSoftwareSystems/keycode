533fe7791b152c5662b8bea7c7168af6a9d807b6
ameera-keyvalue <ameera@keyvalue.systems>
2022-08-18 00:31:06 +0530

    timer

--- src/App.js
+++ src/App.js
@@ -1,22 +1,56 @@
-import logo from './logo.svg';
+import logo from './keycode_logo.svg';
+import keyCode from './keycode.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+
 
 function App() {
+  const FormattedTimeComponent = ({time, label, isLastElement}) => (
+    <div className="RowGroup">
+    <div className="TimeLabel">
+      <p className="TimeLabel-text">{time}</p>
+      <p className="TimeLabel-text">{label}</p>
+    </div>
+    {!isLastElement && <p className="TimeLabel-text">:</p> }
+    </div>
+  );
+
+  const countDownDate = new Date("Aug 28, 2022 09:00:00").getTime();
+
+  const [daysLeft, setDaysLeft] = useState(
+    countDownDate - new Date().getTime(),
+  );
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDaysLeft(countDownDate - new Date().getTime());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [countDownDate]);
+  
+  const getTimeRemaining = () => {
+  const days = Math.floor(daysLeft / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((daysLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((daysLeft % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((daysLeft % (1000 * 60)) / 1000);
+  return { days, hours, minutes, seconds };
+  }
+  const { days, hours, minutes, seconds } = getTimeRemaining();
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="RowGroup">
+          <img src={logo} className="App-logo" alt="logo" />
+          <img src={keyCode} className="App-logo App-text" alt="logo" />
+        </div>
+        <div className="RowGroup">
+          {FormattedTimeComponent({time: days, label: 'Days'})}
+          {FormattedTimeComponent({time: hours, label: 'Hours'})}
+          {FormattedTimeComponent({time: minutes, label: 'Minutes'})}
+          {FormattedTimeComponent({time: seconds, label: 'Seconds', isLastElement: true})}
+        </div>
       </header>
     </div>
   );
