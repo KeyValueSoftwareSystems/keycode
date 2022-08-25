507e981176df107664e51b3fe5e574b51b23b6fb
ameera-keyvalue <ameera@keyvalue.systems>
2022-08-25 17:29:46 +0530

    add animated texts

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 
+import kvLogo from "./kvLogo.svg";
 import logo from "./keycode_logo.svg";
 import keyCode from "./keycode.svg";
 
@@ -17,7 +18,7 @@ function App() {
     </div>
   );
 
-  const countDownDate = new Date("Aug 28, 2022 09:00:00").getTime();
+  const countDownDate = new Date("Aug 27, 2022 09:00:00").getTime();
 
   const [daysLeft, setDaysLeft] = useState(
     countDownDate - new Date().getTime()
@@ -28,57 +29,83 @@ function App() {
       setDaysLeft(countDownDate - new Date().getTime());
     }, 1000);
 
+    startAnimation();
+
     return () => clearInterval(interval);
   }, [countDownDate]);
 
   const getTimeRemaining = () => {
-    const days = Math.floor(daysLeft / (1000 * 60 * 60 * 24));
-    const hours = Math.floor(
-      (daysLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    );
-    const minutes = Math.floor((daysLeft % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((daysLeft % (1000 * 60)) / 1000);
-    return { days, hours, minutes, seconds };
+    if (daysLeft > 0) {
+      const days = Math.floor(daysLeft / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(
+        (daysLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor((daysLeft % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((daysLeft % (1000 * 60)) / 1000);
+      return { days, hours, minutes, seconds };
+    }
+    return { days: 0, hours: 0, minutes: 0, seconds: 0 };
   };
   const { days, hours, minutes, seconds } = getTimeRemaining();
+
+  const startAnimation = () => {
+    const words = document.getElementsByClassName("word");
+    console.log(words);
+    let activeWord = 0;
+    words[activeWord].classList.add("active-word");
+    setInterval(() => {
+      if(activeWord === -1){
+        words[words.length - 1].classList.remove("active-word");
+      } else {
+        words[activeWord].classList.remove("active-word");
+      }
+      activeWord += 1;
+      console.log(activeWord);
+      words[activeWord].classList.add("active-word");
+      if(activeWord === words.length - 1) { activeWord = -1};
+    }, 4000);
+  };
   return (
     <div className="App">
       <header className="App-header">
-      <div className="area" >
-            <ul className="circles">
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-            </ul>
-    </div>
-          <div className="RowGroup">
-            <img src={logo} className="App-logo" alt="logo" />
-            <img src={keyCode} className="App-logo App-text" alt="logo" />
-          </div>
-          <h1>
-            <span></span>
-            <div class="message">
-              <div class="word">250+ Creators</div>
-              <div class="word">25 teams</div>
-              <div class="word">24 hours</div>
-            </div>
-          </h1>
-          <div className="timer">
-            {FormattedTimeComponent({ time: days, label: "Days" })}
-            {FormattedTimeComponent({ time: hours, label: "Hours" })}
-            {FormattedTimeComponent({ time: minutes, label: "Minutes" })}
-            {FormattedTimeComponent({
-              time: seconds,
-              label: "Seconds",
-            })}
-          </div>
+        <div className="area">
+          <ul className="circles">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+        <div className="kv-logo">
+          <img src={kvLogo} className="kv-logo" alt="kv-logo"/>
+        </div>
+        <div className="RowGroup">
+          <img src={logo} className="App-logo" alt="logo" />
+          <img src={keyCode} className="App-logo App-text" alt="logo" />
+        </div>
+        <h1 id="animated-word">
+          <div class="word">24 hours</div>
+          <div class="word">25 teams</div>
+          <div class="word">Neurons on fire</div>
+          <div class="word">Detonating Ideas</div>
+          <div class="word">Choosing 'Code' words</div>
+          <div class="word">A battle set to fight</div>
+        </h1>
+        <div className="timer">
+          {FormattedTimeComponent({ time: days, label: "Days" })}
+          {FormattedTimeComponent({ time: hours, label: "Hours" })}
+          {FormattedTimeComponent({ time: minutes, label: "Minutes" })}
+          {FormattedTimeComponent({
+            time: seconds,
+            label: "Seconds",
+          })}
+        </div>
       </header>
     </div>
   );
